ea77785f57d638031e4817494952e20a6dfe4842
foreA-adoxid <forea.adoxid@gmail.com>
2026-09-26 00:53:42 +0300

    feat: update sdk

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import { modulePlugin } from '@pulsesync/addon-sdk/vite'
 import { defineConfig } from 'vite'
 
-import { apiMajor } from './src/module-api.ts'
-
 export default defineConfig({
-    plugins: [modulePlugin({ apiMajor })],
+    plugins: [modulePlugin()],
 })
